5b8fea860fc4504573b388f9bda49d6e7ad130cb
NaveenChowdary <naveen@naveenchowdary.com>
2021-02-03 14:24:17 +0530

    Demo Added

--- Demo/DemoWebApp/Controllers/HomeController.cs
+++ Demo/DemoWebApp/Controllers/HomeController.cs
@@ -6,6 +6,16 @@ namespace DemoWebApp.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.HtmlString = "";
+            ViewBag.JsonText = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            ViewBag.JsonText = formCollection.Get("txtArea");
+            ViewBag.HtmlString = new Json2HtmlTable.Json2Html().GetJson2Html(ViewBag.JsonText);
             return View();
         }
     }
