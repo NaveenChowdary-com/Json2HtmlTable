63eeabe6b5a62c4563cd726e086d1e2167a984cd
Naveen Chowdary <naveenchowdary564@outlook.com>
2022-12-25 11:05:34 +0530

    NewtonsoftJson 13.0.2 Updated

--- Json2Html.cs
+++ Json2Html.cs
@@ -1,8 +1,8 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Newtonsoft.Json;
 
 namespace Json2HtmlTable
 {
